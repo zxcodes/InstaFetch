62dcc0f143af20a5e1534865f31e72bea189ec95
Mohammed Farmaan <farmaanghouse6@gmail.com>
2022-04-07 07:28:06 +0530

    Initial Commit

---
+++ InstaFetch.py
@@ -0,0 +1,35 @@
+from gazpacho import get, Soup
+import re as regex
+import sys
+
+
+def fetchUserData(username):
+    # Fetch Logic
+    requestURL = f'https://www.instagram.com/{username}/feed/?hl=en'
+    fetchedMarkup = get(requestURL)
+    parsedHTML = Soup(fetchedMarkup)
+    fetchedDescription = parsedHTML.find(
+        "meta", {"property": "og:description"}).attrs['content']
+    findUserData = regex.split('\s', fetchedDescription)
+    userName = findUserData[15]
+    print("\n"+"⚡ Username: " + userName.strip('()') + '\n')
+    userFirstname = findUserData[13]
+    userLastname = findUserData[14]
+    print("⚡ Full Name: " + userFirstname, userLastname + '\n')
+    userFollowers = findUserData[0]
+    print("⚡ Followers: " + userFollowers + '\n')
+    userFollowing = findUserData[2]
+    print("⚡ Following: " + userFollowing + '\n')
+    userPosts = findUserData[4]
+    print("😍 Posts: " + userPosts + '\n')
+    print("🔗 Profile Link: " + requestURL + '\n')
+
+
+# User Input
+enteredUsername = input("\n" + "🔥 Enter Instagram Username: ")
+
+if len(enteredUsername) == 0 or len(enteredUsername) < 2:
+    print("Please enter a username before you continue!" + '\n')
+    sys.exit()
+else:
+    fetchUserData(enteredUsername)
---
+++ README.md
@@ -0,0 +1,15 @@
+# InstaFetch
+
+### A fun little project to scrape profile details from an Instagram account. I made this long ago while I was learning to Scrape the Web.
+
+## How it works?
+
+It uses Gazpacho Scraping Module to make the request as well as parse the markup received from it. We then further split the data using regex module and parse the content from it which finally gets printed out to the console.
+
+## Checkout the Gazpacho library
+
+https://gazpacho.xyz/
+
+# In Action
+
+![image](https://user-images.githubusercontent.com/44538497/162104029-aae47453-50cb-41fe-ae7d-78cf6aa5c185.png)
