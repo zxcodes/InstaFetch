88d2797bd4e057ec23ef4ce2c6431c56b7a7a95c
Mohammed Farmaan <farmaanghouse6@gmail.com>
2022-04-12 09:18:04 +0530

    Handled error when accounts aren't found

--- InstaFetch.py
+++ InstaFetch.py
@@ -6,26 +6,30 @@ import sys
 def fetchUserData(username):
     # Fetch Logic
     requestURL = f'https://www.instagram.com/{username}/feed/?hl=en'
-    fetchedMarkup = get(requestURL)
-    parsedHTML = Soup(fetchedMarkup)
-    fetchedDescription = parsedHTML.find(
-        "meta", {"property": "og:description"}).attrs['content']
-    findUserData = regex.split('\s', fetchedDescription)
-    userName = findUserData[15]
-    print("\n"+"⚡ Username: " + userName.strip('()') + '\n')
-    userFirstname = findUserData[13]
-    userLastname = findUserData[14]
-    print("⚡ Full Name: " + userFirstname, userLastname + '\n')
-    userFollowers = findUserData[0]
-    print("⚡ Followers: " + userFollowers + '\n')
-    userFollowing = findUserData[2]
-    print("⚡ Following: " + userFollowing + '\n')
-    userPosts = findUserData[4]
-    print("😍 Posts: " + userPosts + '\n')
-    print("🔗 Profile Link: " + requestURL.strip("/feed/?hl=en") + '\n')
+    try:
+        fetchedMarkup = get(requestURL)
+    except:
+        print("\n😖 Account not found! \n   Check the username or try another one!\n")
+    else:
+        parsedHTML = Soup(fetchedMarkup)
+        fetchedDescription = parsedHTML.find(
+            "meta", {"property": "og:description"}).attrs['content']
+        findUserData = regex.split('\s', fetchedDescription)
+        userName = findUserData[15]
+        print("\n"+"⚡ Username: " + userName.strip('()') + '\n')
+        userFirstname = findUserData[13]
+        userLastname = findUserData[14]
+        print("⚡ Full Name: " + userFirstname, userLastname + '\n')
+        userFollowers = findUserData[0]
+        print("⚡ Followers: " + userFollowers + '\n')
+        userFollowing = findUserData[2]
+        print("⚡ Following: " + userFollowing + '\n')
+        userPosts = findUserData[4]
+        print("😍 Posts: " + userPosts + '\n')
+        print("🔗 Profile Link: " + requestURL.strip("/feed/?hl=en") + '\n')
 
 
-# User Input
+# Getting user input
 enteredUsername = input("\n" + "🔥 Enter Instagram Username: ")
 
 if len(enteredUsername) == 0 or len(enteredUsername) < 2:
